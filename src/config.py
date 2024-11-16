68a4d015a56007d7d74383fe4308928cec7a988e
NawarTurk <nawar.turk@mail.concordia.ca>
2024-11-15 20:06:31 -0500

    cleanup: Organize results, add new prompts and tests

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 
 # API keys
-OPENAI_API_KEY = "API key"
-GEMINI_API_KEY = "API key"
+OPENAI_API_KEY = "key"
+GEMINI_API_KEY = "key"
 CLAUDE_API_KEY = "your_claude_api_key_here"
 
 # Prompt 
-PROMPT_KEY = 'N3_prompt'
+PROMPT_KEY = 'N3_prompt+'
 
 # Parser
 PARSER_ID = 2
@@ -13,11 +13,14 @@ PARSER_ID = 2
 #  2 for the gemini_prompt_handler   
 #  3 for the claude_prompt_handler   NOT DONE YET
 
+#  11 for multi_level_gemini_prompt_handler_v1
+#  12 for multi_level_gemini_prompt_handler_v2
+
 #Delay
-PROCESSING_DELAY_SECONDS = 5
+PROCESSING_DELAY_SECONDS = 10
 
 # Model names
-OPENAI_MODEL_NAME = "gpt-4o"
+OPENAI_MODEL_NAME = "gpt-4o" 
 # ['tts-1', 'tts-1-1106', 'gpt-4-turbo-2024-04-09', 'gpt-4o', 'gpt-4o-2024-08-06', 'dall-e-2', 'whisper-1', 'gpt-3.5-turbo-instruct', 'tts-1-hd-1106', 'gpt-3.5-turbo', 'gpt-3.5-turbo-0125', 'babbage-002', 'gpt-4o-2024-05-13', 'davinci-002', 'dall-e-3', 'gpt-4o-realtime-preview', 'gpt-4o-realtime-preview-2024-10-01', 'tts-1-hd', 'chatgpt-4o-latest', 'gpt-4-turbo-preview', 'gpt-4-1106-preview', 'text-embedding-ada-002', 'gpt-3.5-turbo-16k', 'gpt-4-turbo', 'text-embedding-3-small', 'text-embedding-3-large', 'gpt-4o-mini-2024-07-18', 'gpt-3.5-turbo-1106', 'gpt-4o-mini', 'gpt-4o-audio-preview', 'gpt-4o-audio-preview-2024-10-01', 'gpt-4-0613', 'gpt-4-0125-preview', 'gpt-4', 'gpt-3.5-turbo-instruct-0914']
 # +-----------------+-------------------------------------------------------------------------------------------------+
 # | Model           | Description                                                                                     |
