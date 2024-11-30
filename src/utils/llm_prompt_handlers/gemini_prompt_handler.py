6bf5d62fe0b18ff6205793dfa592d576a96a4bde
NawarTurk <nawar.turk@mail.concordia.ca>
2024-11-29 19:47:36 -0500

    add: include GPT-based discourse relation parsing results and apply minor unrelated modifications

--- src/utils/llm_prompt_handlers/gemini_prompt_handler.py
+++ src/utils/llm_prompt_handlers/gemini_prompt_handler.py
@@ -9,6 +9,5 @@ genai.configure(api_key=api_key)
 def gemini_prompt_handler(paragraph, prompt_template):
     prompt = prompt_template.format(paragraph=paragraph)
     model = genai.GenerativeModel(model_name)
-    print(model)
     response = model.generate_content(prompt)
     return (response.text).strip()
