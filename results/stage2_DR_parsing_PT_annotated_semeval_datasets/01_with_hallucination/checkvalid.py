e2ee2c677ed560995f2f5141d14a19436bb51d57
NawarTurk <nawar.turk@mail.concordia.ca>
2024-12-10 17:03:26 -0500

    Add: Processed PT dataset with DR relations.
    - Renamed combined PT datasets for clarity (PT_dataset_1_*).
    - Added new processed datasets

---
+++ results/stage2_DR_parsing_PT_annotated_semeval_datasets/01_with_hallucination/checkvalid.py
@@ -0,0 +1,13 @@
+import json
+
+DR_level2 = {
+        "synchronous", "asynchronous", "cause", "cause+belief", "cause+speechact", "condition", "condition+speechact", "negative-condition", 
+        "negative-condition+speechact", "purpose", "concession", "concession+speechact", "contrast", "similarity", "conjunction", "disjunction", 
+        "equivalence", "exception", "instantiation", "level-of-detail", "manner", "substitution"}
+
+with open('PT_dataset_1_gemini-1.5-pro_promptN02.json', 'r', encoding='utf-8') as jsonfile:
+    data = json.load(jsonfile)
+
+for key, value in data.items():
+    if value['DR'] not in DR_level2:
+        print(f'{value['DR']} ____ {key}')
