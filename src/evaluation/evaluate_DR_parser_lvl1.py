1c11e0a833c83f15ddb6b197dc97fe593e04cc9d
NawarTurk <nawar.turk@mail.concordia.ca>
2024-12-19 20:21:53 -0500

    feat: add avg F1 calculation and run experiments on fine-tuned Gemini-1.5-flash-001 (prompts N01-N10)

--- src/evaluation/evaluate_DR_parser_lvl1.py
+++ src/evaluation/evaluate_DR_parser_lvl1.py
@@ -13,10 +13,12 @@ dataset_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126
 output_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "metrics_level1")
 summary_report_xlsx_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level1_metrics_summary.xlsx")
 summary_avg_report_xlsx_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level1_avg_metrics_summary.xlsx")
+average_f1_scores_level1_per_parser = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "average_f1_scores_level1_per_parser.json")
 plot_output_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level1_model_f1_macro_by_prompt.png")
 plot_avg_output_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level1_model_avg_f1_macro_by_prompt.png")
 
-def get_level1_id(level1_name):
+
+def get_level1_DR_id(level1_name):
     # Define a dictionary to map level-1 categories to numbers
     level1_to_number = {
         "temporal": 1,
@@ -26,21 +28,28 @@ def get_level1_id(level1_name):
     }
     return level1_to_number.get(level1_name, None)
 
+# Add a numeric ID column to the DataFrame
+def assign_numeric_id(prompt_id):
+    if 'N' in prompt_id:
+        # Extract the number after 'N' and return as integer
+        return int(prompt_id.replace('promptN', ''))
+
 
 def evaluate_DR_parser_with_f1_lvl1(): 
     summary_report = {}
     avg_f1_values = {}
+    global_class_f1_scores = {}
+
 
     for filename in os.listdir(dataset_path):
-
         if filename.endswith('.json'):
             input_file= os.path.join(dataset_path, filename)
 
             with open(input_file, 'r', encoding='utf-8') as jsonfile:
 
                 dataset = json.load(jsonfile)
-                dr_true = [get_level1_id(sample['actual_DR_level1'].lower()) for _, sample in dataset.items()]
-                dr_pred = [get_level1_id(get_level1_DR(sample['predicted_DR_level2'])) for _, sample in dataset.items()]
+                dr_true = [get_level1_DR_id(sample['actual_DR_level1'].lower()) for _, sample in dataset.items()]
+                dr_pred = [get_level1_DR_id(get_level1_DR(sample['predicted_DR_level2'])) for _, sample in dataset.items()]
          
                 f1_macro = f1_score(dr_true, dr_pred, average='macro')
                 f1_micro = f1_score(dr_true, dr_pred, average='micro')
@@ -87,7 +96,7 @@ def evaluate_DR_parser_with_f1_lvl1():
                     'adjusted_accuracy': adjusted_accuracy
                 }
 
-                key = f'{model_name}-{prompt_id}'
+                key = f'{model_name}_{prompt_id}'
                 if key not in avg_f1_values:
                     avg_f1_values[key] = {
                         'f1_macro_values': [],
@@ -98,43 +107,46 @@ def evaluate_DR_parser_with_f1_lvl1():
                 avg_f1_values[key]['prompt_id'] = prompt_id
                 avg_f1_values[key]['model'] = model_name
 
+                    ###
+                report_dict = classification_report(dr_true, dr_pred, digits=3, output_dict=True)
+                f1_by_class = {class_label: metrics['f1-score'] 
+                    for class_label, metrics in report_dict.items() 
+                    if class_label.isdigit()}
+                
+                if key not in global_class_f1_scores:
+                    global_class_f1_scores[key] = {class_label: {'f1_values': []} for class_label in f1_by_class}
+
+                for class_label, f1_score_value in f1_by_class.items():
+                    global_class_f1_scores[key][class_label]['f1_values'].append(f1_score_value)
+    
+    for parser, class_labels in global_class_f1_scores.items():
+        for class_label, value in class_labels.items():
+             value['average_f1_score'] = sum(value['f1_values']) / len(value['f1_values'])
+        
     for key, value in avg_f1_values.items():
         value['average_f1_macro'] = sum(value['f1_macro_values'])/len(value['f1_macro_values'])
         value['average_accuracy'] = sum(value['accuracy_values'])/len(value['accuracy_values'])
 
-
-
     df = pd.DataFrame.from_dict(summary_report, orient= 'index')
     df_avg = pd.DataFrame.from_dict(avg_f1_values, orient = 'index')
-
-
-
-
-    # Add a numeric ID column to the DataFrame
-    def assign_numeric_id(prompt_id):
-        if 'N' in prompt_id:
-            # Extract the number after 'N' and return as integer
-            return int(prompt_id.replace('promptN', ''))
-
     # Apply the function to create the numeric_id column
     df['numeric_id'] = df['prompt_id'].apply(assign_numeric_id)
     df_avg['numeric_id'] = df_avg['prompt_id'].apply(assign_numeric_id)
-
     # Sort the DataFrame by the numeric_id
     df = df.sort_values('numeric_id')
     df_avg = df_avg.sort_values('numeric_id')
-
     df.to_excel(summary_report_xlsx_path, index=False)
     df_avg.to_excel(summary_avg_report_xlsx_path, index=False)
-    
+
+    with open(average_f1_scores_level1_per_parser, 'w') as f:
+        json.dump(global_class_f1_scores, f, indent=4)
+
     # Plotting
     plt.figure(figsize=(12, 8))
-
      # Plot each model as a separate line
     for model in df['model'].unique():
         subset = df[df['model'] == model]
         plt.plot(subset['prompt_id'], subset['f1_macro'], marker='o', label=model)
-
     # Customizing the plot
     plt.xlabel('Prompt ID')
     plt.ylabel('F1 Macro')
@@ -144,7 +156,7 @@ def evaluate_DR_parser_with_f1_lvl1():
     plt.tight_layout()  # Adjust layout to prevent label clipping
     plt.ylim(0, 0.8)
     plt.savefig(plot_output_path, format='png', dpi=300)
-    plt.show()
+    # plt.show()
 
     plt.figure(figsize=(12, 8))
     for model in df_avg['model'].unique():
@@ -158,7 +170,7 @@ def evaluate_DR_parser_with_f1_lvl1():
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig(f'{plot_avg_output_path}', format ='png', dpi=300)
-    plt.show()
+    # plt.show()
 
 
     
--- src/evaluation/evaluate_DR_parser_lvl2.py
+++ src/evaluation/evaluate_DR_parser_lvl2.py
@@ -1,7 +1,7 @@
 import os
 import json
 import pandas as pd
-from utils.helpers.pt_dr_getters import get_DR_id_by_name
+from utils.helpers.pt_dr_getters import get_level2_DR_id_by_name
 import warnings
 from sklearn.metrics import f1_score, classification_report, accuracy_score
 from sklearn.exceptions import UndefinedMetricWarning
@@ -14,12 +14,20 @@ dataset_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126
 output_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "metrics_level2")
 summary_report_xlsx_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level2_metrics_summary.xlsx")
 summary_avg_report_xlsx_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level2_avg_metrics_summary.xlsx")
+average_f1_scores_level2_per_parser = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "average_f1_scores_level2_per_parser.json")
 plot_output_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level2_model_f1_macro_by_prompt.png")
 plot_avg_output_path = os.path.join("..", "results", "stage1_eval_initial_DR_prompts_126instances", "summary_report", "level2_model_avg_f1_macro_by_prompt.png")
 
+# Add a numeric ID column to the DataFrame
+def assign_numeric_id(prompt_id):
+    if 'N' in prompt_id:
+        # Extract the number after 'N' and return as integer
+        return int(prompt_id.replace('promptN', ''))
+    
 def evaluate_DR_parser_with_f1(): 
     summary_report = {}
     avg_f1_values = {}
+    global_class_f1_scores = {}
 
 
     for filename in os.listdir(dataset_path):
@@ -29,15 +37,13 @@ def evaluate_DR_parser_with_f1():
             with open(input_file, 'r', encoding='utf-8') as jsonfile:
 
                 dataset = json.load(jsonfile)
-
-                dr_true = [get_DR_id_by_name(sample['actual_DR_level2']) for _, sample in dataset.items()]
-                dr_pred = [get_DR_id_by_name(sample['predicted_DR_level2']) for _, sample in dataset.items()]
+                dr_true = [get_level2_DR_id_by_name(sample['actual_DR_level2'].lower()) for _, sample in dataset.items()]
+                dr_pred = [get_level2_DR_id_by_name(sample['predicted_DR_level2']) for _, sample in dataset.items()]
 
                 f1_macro = f1_score(dr_true, dr_pred, average='macro')
                 f1_micro = f1_score(dr_true, dr_pred, average='micro')
                 f1_weighted = f1_score(dr_true, dr_pred, average='weighted')
                 accuracy = accuracy_score(dr_true, dr_pred)
-
 
                 report = classification_report(dr_true, dr_pred, digits=3)
 
@@ -62,8 +68,6 @@ def evaluate_DR_parser_with_f1():
                 prompt_id = filename_segments[0]
                 model_name = filename_segments[1]
 
-
-
                 # Accounting for hallucinations that were removed manually from some experiments
                 numb_faulty_results = 126 - len(dataset)
                 num_correct_predictions = accuracy * len(dataset)
@@ -81,7 +85,7 @@ def evaluate_DR_parser_with_f1():
                     'adjusted_accuracy': adjusted_accuracy
                 }
 
-                key = f'{model_name}-{prompt_id}'
+                key = f'{model_name}_{prompt_id}'
                 if key not in avg_f1_values:
                     avg_f1_values[key] = {
                         'f1_macro_values': [],
@@ -91,6 +95,26 @@ def evaluate_DR_parser_with_f1():
                 avg_f1_values[key]['accuracy_values'].append(accuracy)
                 avg_f1_values[key]['prompt_id'] = prompt_id 
                 avg_f1_values[key]['model'] = model_name 
+
+                    ###
+                report_dict = classification_report(dr_true, dr_pred, digits=3, output_dict=True)
+                f1_by_class = {class_label: metrics['f1-score'] 
+                    for class_label, metrics in report_dict.items() 
+                    if class_label.isdigit()}
+                
+                if key not in global_class_f1_scores:
+                    global_class_f1_scores[key] = {class_label: {'f1_values': []} for class_label in f1_by_class}
+
+                for class_label, f1_score_value in f1_by_class.items():
+                    try:
+                        global_class_f1_scores[key][class_label]['f1_values'].append(f1_score_value)
+                    except:
+                        continue
+                  
+    for parser, class_labels in global_class_f1_scores.items():
+        for class_label, value in class_labels.items():
+             value['average_f1_score'] = sum(value['f1_values']) / len(value['f1_values'])
+
     
     for key, value in avg_f1_values.items():
         value['average_f1_macro'] = sum(value['f1_macro_values'])/len(value['f1_macro_values'])
@@ -98,29 +122,18 @@ def evaluate_DR_parser_with_f1():
 
     df = pd.DataFrame.from_dict(summary_report, orient = 'index')
     df_avg = pd.DataFrame.from_dict(avg_f1_values, orient = 'index')
-
-    df.to_excel(summary_report_xlsx_path, index=False)
-    df_avg.to_excel(summary_avg_report_xlsx_path, index =False)
-
-
-    # Add a numeric ID column to the DataFrame
-    def assign_numeric_id(prompt_id):
-        if 'N' in prompt_id:
-            # Extract the number after 'N' and return as integer
-            return int(prompt_id.replace('promptN', ''))
-
-
-
     # Apply the function to create the numeric_id column
     df['numeric_id'] = df['prompt_id'].apply(assign_numeric_id)
     df_avg['numeric_id'] = df_avg['prompt_id'].apply(assign_numeric_id)
-
     # Sort the DataFrame by the numeric_id
     df = df.sort_values('numeric_id')
     df_avg = df_avg.sort_values('numeric_id')
-    
     df.to_excel(summary_report_xlsx_path, index=False)
     df_avg.to_excel(summary_avg_report_xlsx_path, index =False)
+
+    with open(average_f1_scores_level2_per_parser, 'w') as f:
+        json.dump(global_class_f1_scores, f, indent=4)
+
 
     plt.figure(figsize=(12, 8))
     # Plot each model as a separate line
@@ -136,7 +149,7 @@ def evaluate_DR_parser_with_f1():
     plt.tight_layout()  # Adjust layout to prevent label clipping
     plt.ylim(0, 0.8)
     plt.savefig(plot_output_path, format='png', dpi=300)
-    plt.show()
+    # plt.show()
 
     plt.figure(figsize=(12,8))
     for model in df_avg['model'].unique():
@@ -150,7 +163,7 @@ def evaluate_DR_parser_with_f1():
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig(f'{plot_avg_output_path}', format ='png', dpi=300)
-    plt.show()
+    # plt.show()
 
 
     
